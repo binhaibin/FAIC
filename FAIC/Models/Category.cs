bc19e0fcea05fb845c898dc111c9a5f7c63730ad
unknown <binhaibin>
2018-11-24 10:00:25 +0700

    2

---
+++ FAIC/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace FAIC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public List<Product> Product { get; set; }
+    }
+}
---
+++ FAIC/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace FAIC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public String ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public int UnitPrice { get; set; }
+        public String UnitsInStock { get; set; }
+        public String UnitsOnorder { get; set; }
+        public String ReorderLevel { get; set; }
+        public String Discontinued { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+
+    }
+}
